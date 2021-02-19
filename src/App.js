693bc7acc2cfa8c4823df889ee7b2ed3aef0e79a
WalkerMozes <mozeswalker@outlook.com>
2021-02-19 16:53:17 +0000

    implemented particle.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
-import Navigation from './components/Navigation';
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Navigation from "./components/Navigation";
+import Landing from "./components/Landing";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-  render() { 
+  render() {
     return (
-      <div>
+      <div className="App">
+        <Particles
+          style={{ position: "absolute" }}
+          height="95%"
+          width="95%"
+          params={{
+            particles: {
+              color: {
+                value: "#000000"
+              },
+              line_linked: {
+                color: {
+                  value: "#000000"
+                }
+              },
+              number: {
+                value: 50
+              },
+              size: {
+                value: 3
+              }
+            }
+          }}
+        />
         <Navigation />
+        <Landing />
       </div>
-
     );
   }
 }
- 
-export default App;
 
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="container">
+            <h1>Hi, I'm Mozes...</h1>
+        </div>
+    )
+}
+
+export default Landing; 
